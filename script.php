54fdd2df0aef187fa6dc8ab25194fab37ec8d713
denimskirty <165702230+denimskirty@users.noreply.github.com>
2024-04-19 11:34:31 -0700

    Update script.php

--- script.php
+++ script.php
@@ -1,9 +1,1 @@
-
-if(isset($_POST['submit'])) 
-{ 
-	$to= "denimskirty@gmail.com"; 
-	$subject= "a question"; 
-	$msg= $_POST["value1"]; //textarea value will be send as message here. 
-	$header= "Header for mail" //For example: From: abc@xyz.com 
-	mail($to,$subject,$msg,$header); 
-} 
+<?php mail('email@domain.com','mail subject',$_POST['msg']);?>
